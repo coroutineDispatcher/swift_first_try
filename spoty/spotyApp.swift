0e1a9530f73afb0b18b49d3657999737c0021b4c
coroutinedispatcher <stavro96@gmail.com>
2022-06-12 17:07:10 +0200

    Initial Commit

---
+++ spoty/spotyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotyApp.swift
+//  spoty
+//
+//  Created by Stavro Xhardha on 12.06.22.
+//
+
+import SwiftUI
+
+@main
+struct spotyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
